d56b36ba07d254fcb27f2daec1bc71fe4039b2d2
gazz0008 <gazz0008@algonquinlive.com>
2015-05-09 15:41:44 -0400

    For real, added some base framework for the UI
    Created script to hold button events

---
+++ RapBattle/Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public void ResolveClick(Object sender)
+    {   
+        GameObject gO = sender as GameObject;
+
+        Button button = gO.GetComponent<Button>();
+        
+        if (button == null)
+        {
+            Debug.Log("button null");
+            return;
+        }
+
+        button.GetComponentInChildren<Text>().text = "happy";
+    }
+}
