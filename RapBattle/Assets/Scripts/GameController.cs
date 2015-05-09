a8e870a6ec9149ace7e18446e30d175e931ff33a
gazz0008 <gazz0008@algonquinlive.com>
2015-05-09 17:39:18 -0400

    Everything is happening!
    Slider for hype meter

--- RapBattle/Assets/Scripts/GameController.cs
+++ RapBattle/Assets/Scripts/GameController.cs
@@ -18,16 +18,44 @@ public class GameController : MonoBehaviour
     public Color minHypeColor;
 
     public Slider hypeMeter;
+    public Image hypeMeterBackground;
 
-	// Use this for initialization
+    public int currentHype;
+
+    public int hypeIncrement = 5;
+	
+    
+    // Use this for initialization
 	void Start ()
     {
-	
+        hypeMeter.fillRect.GetComponent<Image>().color = maxHypeColor;
+        hypeMeterBackground.color = minHypeColor;
+        hypeMeter.minValue = 0;
+        hypeMeter.maxValue = maxHypeValue;
+        currentHype = (int)(maxHypeValue * 0.5f);
+        updateHypeMeter();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("getting hype!");
+            currentHype += hypeIncrement;
+            updateHypeMeter();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("getting unhype!");
+            currentHype -= hypeIncrement;
+            updateHypeMeter();
+        }
 	}
+
+    private void updateHypeMeter()
+    {
+        hypeMeter.value = currentHype;
+    }
 }
